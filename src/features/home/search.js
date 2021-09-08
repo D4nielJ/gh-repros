33809587be3849b5425d428d160e3df22a318506
d4nielj <d4niel.djm@gmail.com>
2021-09-07 21:42:35 -0500

    Handle user input

--- src/features/home/search.js
+++ src/features/home/search.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
+
 const Search = () => {
-  return ( 
+  const [user, setUser] = useState('');
+  const handleUser = (e) => {
+    setUser(e.target.value);
+  };
+
+  return (
     <form>
-      <input type="text" name="search" id="search" />
+      <input type="text" name="search" id="search" value={user} onChange={handleUser} />
       <button type="submit">Search user</button>
     </form>
-   );
-}
- 
+  );
+};
+
 export default Search;
