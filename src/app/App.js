ab46e23791c80f3c1d5f7732ee5d9b2333fe48ea
d4nielj <d4niel.djm@gmail.com>
2021-09-08 12:50:56 -0500

    Add react-router-dom and basic routing

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,20 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Search from '../features/home/search';
+import User from '../features/user/user';
 
 const App = () => (
-  <div className="App">
-    <Search />
-  </div>
+  <Router basename={process.env.PUBLIC_URL}>
+    <div className="App">
+      <Switch>
+        <Route path="/user">
+          <User />
+        </Route>
+        <Route exact path="/">
+          <Search />
+        </Route>
+      </Switch>
+    </div>
+  </Router>
 );
 
 export default App;
--- src/features/user/user.js
+++ src/features/user/user.js
@@ -2,7 +2,7 @@ import { useSelector } from 'react-redux';
 
 const User = () => {
   const login = useSelector((state) => state.user.user.login);
-  return <section>{login && <h1>{login}</h1>}</section>;
+  return <section>{login && <h2>{login}</h2>}</section>;
 };
 
 export default User;
