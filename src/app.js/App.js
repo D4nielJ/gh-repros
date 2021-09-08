4607c27fdd0787db1412a8cf1b231642ef0906bc
d4nielj <d4niel.djm@gmail.com>
2021-09-07 21:36:39 -0500

    Add linters dependecies

--- src/app.js/App.js
+++ src/app.js/App.js
@@ -1,12 +1,9 @@
 import Search from "../features/home/search"
 
-const example = process.env.REACT_APP_EXAMPLE;
-
 const App = () => {
   return (
     <div className="App">
       <Search />
-      <div>{example}</div>
     </div>
   );
 }
