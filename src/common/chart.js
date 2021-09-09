31a07df67aaf31f2a0a3c6b11514aa5ad8c849fc
d4nielj <d4niel.djm@gmail.com>
2021-09-09 17:23:49 -0500

    Add some hover effects in repo

--- src/common/chart.js
+++ src/common/chart.js
@@ -1,5 +1,42 @@
+import { Pie } from 'react-chartjs-2';
+
 const Chart = ({ data }) => {
-  return <div className="flex">Hello world! {data && <div>{data[0].label}</div>} </div>;
+  const labels = data.map((lang) => lang.label);
+  const values = data.map((lang) => lang.value);
+  const colors = data.map((lang) => lang.color);
+
+  const chartData = {
+    labels,
+    datasets: [
+      {
+        labels,
+        data: values,
+        backgroundColor: colors,
+        hoverOffset: 1,
+      },
+    ],
+  };
+
+  const options = {
+    cutout: '50%',
+    responsive: true,
+    borderWidth: 1,
+    tooltips: {
+      cornerRadius: 3,
+    },
+    plugins: {
+      legend: {
+        position: 'bottom',
+        align: 'center',
+      },
+    },
+  };
+
+  return (
+    <div>
+      <Pie data={chartData} options={options} />
+    </div>
+  );
 };
 
 export default Chart;
--- src/features/repo/repo.js
+++ src/features/repo/repo.js
@@ -9,6 +9,24 @@ import Chart from '../../common/chart';
 
 const token = process.env.REACT_APP_PTOKEN;
 
+const exampleValues = [
+  {
+    label: 'JavaScript',
+    value: 22549,
+    color: '#f1e05a',
+  },
+  {
+    label: 'SCSS',
+    value: 6722,
+    color: '#c6538c',
+  },
+  {
+    label: 'HTML',
+    value: 1496,
+    color: '#e34c26',
+  },
+];
+
 const Repo = ({ owner, name }) => {
   const dispatch = useDispatch();
   const repo = useSelector((state) => state.repo.repo);
@@ -21,18 +39,22 @@ const Repo = ({ owner, name }) => {
     dispatch(fetchRepo(url));
   }, [owner, name]);
 
-  const me = new GhPolyglot(url);
-
   useEffect(() => {
-    me.repoStats((err, stats) => {
-      if (err) {
-        console.error('Error:', err);
-        setError({ active: true, type: 400 });
-      }
-      console.log(stats);
-      setRepoData(stats);
-    });
-  }, [owner, name]);
+    setRepoData(exampleValues);
+  }, []);
+
+  // const me = new GhPolyglot(url);
+
+  // useEffect(() => {
+  //   me.repoStats((err, stats) => {
+  //     if (err) {
+  //       console.error('Error:', err);
+  //       setError({ active: true, type: 400 });
+  //     }
+  //     console.log(stats);
+  //     setRepoData(stats);
+  //   });
+  // }, [owner, name]);
 
   return (
     <section className="min-h-screen bg-bh-lightBlue text-white">
@@ -40,14 +62,20 @@ const Repo = ({ owner, name }) => {
         <>
           <Navbar title={repo.name} />
           <article className="flex flex-col items-end px-4 pt-8">
-            <div className="flex items-center mb-4">
-              <h2 className="font-black text-2xl text-right mr-2">{repo.full_name}</h2>
-              <a target="_blank" href={repo.html_url}>
-                <MarkGithubIcon size={20} className="" />
+            <div className="mb-4">
+              <a target="_blank" href={repo.html_url} className="flex items-center hover:text-gray-200 transition-all">
+                <h2 className="font-black text-2xl text-right mr-2">{repo.full_name}</h2>
+                <MarkGithubIcon size={20} />
               </a>
             </div>
-            <p className="max-w-prose text-right mb-4">{repo.description}</p>
-            {repoData ? <Chart data={repoData} /> : <p>Number of requests over the limit.</p>}
+            <p className="max-w-prose text-right mb-8">{repo.description}</p>
+            {repoData ? (
+              <div className="w-full mb-8 self-center bg-white px-6 pt-6 pb-3 rounded-xl shadow">
+                <Chart data={repoData} />
+              </div>
+            ) : (
+              <p>Number of requests over the limit.</p>
+            )}
             <div className="mb-1">
               <EyeIcon className="mr-1" />
               <span className="mr-2">{repo.watchers_count}</span>
@@ -57,10 +85,10 @@ const Repo = ({ owner, name }) => {
               <span className="mr-2">{repo.forks_count}</span>
             </div>
             <div className="mb-2">{(repo.size / 1000).toFixed(2)} kb</div>
-            <div className="mb-1">{repo.license.name}</div>
+            {repo.license && <div className="mb-1">{repo.license.name}</div>}
             {repo.owner && (
-              <a target="_blank" href={repo.owner.html_url} className="underline text-lg font-bold">
-                @{repo.owner.login}
+              <a target="_blank" href={repo.owner.html_url} className="underline text-lg font-bold hover:text-gray-200 transition-all">
+                Owner: @{repo.owner.login}
               </a>
             )}
           </article>
