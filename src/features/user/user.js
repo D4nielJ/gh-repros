74e9de1d270ef73616cd87f3b1ba2ad0b7b7d554
d4nielj <d4niel.djm@gmail.com>
2021-09-09 19:41:50 -0500

    Add filter functionality

--- src/features/user/user.js
+++ src/features/user/user.js
@@ -1,15 +1,15 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { PropTypes } from 'prop-types';
 import { useDispatch, useSelector } from 'react-redux';
 import { RepoIcon } from '@primer/octicons-react';
 import { fetchRepos, fetchUser } from './userSlice';
-import Stats from '../../common/stats/stats';
+import Stats from '../../common/stats';
 import Navbar from '../navbar/navbar';
 import RepoPreview from './repoPreview';
 
 const User = ({ id }) => {
-  const user = useSelector((state) => state.user.user);
   const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.user);
   const repos = useSelector((state) => state.user.repos);
 
   useEffect(() => {
@@ -19,23 +19,53 @@ const User = ({ id }) => {
     }
   }, [id]);
 
-  const reposLi = repos.map((repo) => <RepoPreview key={repo.id} repo={repo} />);
+  const [sortType, setSortType] = useState('Stars');
+  const [reposSorted, setReposSorted] = useState([]);
+
+  const handleSortType = (e) => {
+    setSortType(e.target.value);
+  };
+
+  const getTopRepos = (type) => {
+    const map = {
+      Stars: 'stargazers_count',
+      Forks: 'forks_count',
+      Size: 'size',
+    };
+    const sortProperty = map[type];
+    const sorted = repos
+      .filter((repo) => !repo.fork)
+      .sort((a, b) => b[sortProperty] - a[sortProperty]);
+
+    setReposSorted(sorted);
+  };
+
+  useEffect(() => {
+    if (repos.length) {
+      getTopRepos();
+    }
+  }, []);
+
+  useEffect(() => {
+    getTopRepos(sortType);
+  }, [sortType]);
+
+  const reposLi = reposSorted.map((repo) => <RepoPreview key={repo.id} repo={repo} />);
 
   return (
     <section className="min-h-screen bg-bh-lightBlue">
-
       <Navbar title={user.name} />
 
       {user && (
-
         <div className="flex">
           {user.avatar_url && (
-            <div className="w-44 py-4 pl-4">
+            <div className="w-44 py-4 pl-6">
               <div className="w-36 h-36">
-                <div className="aspect-h-1 aspect-w-1 relative rounded-full border-4 border-bh-darkBlue">
+                <div className="aspect-h-1 aspect-w-1 relative rounded-full border-2 border-bh-darkBlue">
                   <img
                     alt={id}
-                    className="bg-bh-blue mix-blend-overlay inset-0 w-full h-full object-cover absolute rounded-full"
+                    className="bg-bh-blue inset-0 w-full h-full object-cover absolute rounded-full"
+                    // className="bg-bh-blue mix-blend-overlay inset-0 w-full h-full object-cover absolute rounded-full"
                     src={user.avatar_url}
                   />
                 </div>
@@ -48,11 +78,13 @@ const User = ({ id }) => {
               <h2 className="font-bold text-white text-2xl leading-tight">{user.name}</h2>
             )}
             {user.html_url && (
-              <a className="text-white underline mb-2 text-sm hover:text-gray-200 transition-all" href={user.html_url} target="_blank" rel="noreferrer">
-                <h3>
-                  @
-                  {user.login}
-                </h3>
+              <a
+                className="text-white underline mb-2 text-sm hover:text-gray-200 transition-all"
+                href={user.html_url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <h3>@{user.login}</h3>
               </a>
             )}
             {user && <Stats followers={user.followers} following={user.following} />}
@@ -61,11 +93,18 @@ const User = ({ id }) => {
       )}
 
       <div className="px-4 py-1 text-white  bg-bh-blue flex justify-between items-center">
-        <h3 className="font-bold uppercase">Repositories</h3>
-        <span>
-          <RepoIcon size={16} className="mr-1" />
-          <span className="font-bold">{user.public_repos}</span>
-        </span>
+        <div class="flex">
+          <span className="mr-1">
+            <RepoIcon size={16} className="mr-1" />
+            <span className="font-bold">{user.public_repos}</span>
+          </span>
+          <h3 className="uppercase">Repos</h3>
+        </div>
+        <select className="text-bh-black" value={sortType} onChange={(e) => handleSortType(e)}>
+          <option value="Stars">Stars</option>
+          <option value="Forks">Forks</option>
+          <option value="Size">Size</option>
+        </select>
       </div>
       <ul className="repos grid grid-cols-2">{reposLi}</ul>
     </section>
